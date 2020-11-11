02b0edba785d528deb1bafbf02209d16eef25d38
Dain Brownlow <dain.brownlow@outlook.com>
2020-11-11 22:08:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 def game_hash()
   game_data = {:home => {
-    :team_name => 'str',:colors => ['aos'], :players => [{},{}]
+    :team_name => 'str',:colors => ['aos'], :players => [{:player_name => s, :number => s, :shoe => s, :points => s, :rebounds => s, :assists => s, :steals => s, :blocks => s, :slam_dunks => s},{}]
   },:away => {
     :team_name => 'str',:colors => ['aos'], :players => [{},{}]
   }}
